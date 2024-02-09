5f0b15c67cdd36816d015b6d8935c73f19248f97
Ouhadj meriem <ouhadjmeriem2427@gmail.com>
2024-02-09 20:34:35 +0100

    fixed fonts issues

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+import SideNav from '@/app/ui/dashboard/sidenav';
+ 
+
+/* This child can either be a page or another layout. In this case, the pages inside 
+/dashboard will automatically be nested inside a <Layout /> */
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
